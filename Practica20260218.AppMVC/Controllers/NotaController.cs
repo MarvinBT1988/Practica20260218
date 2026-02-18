dc6643ed3451b008e30ce0d11d31624be1cad191
Marvin Antonio BarreraTrigueros <antoniobtrigueros@gmail.com>
2026-02-18 11:19:51 -0600

    SCRUM-11 Programar el controlador para hacer el calculo de notas

--- Practica20260218.AppMVC/Controllers/NotaController.cs
+++ Practica20260218.AppMVC/Controllers/NotaController.cs
@@ -7,6 +7,13 @@ namespace Practica20260218.AppMVC.Controllers
         public IActionResult Index()
         {
             return View();
-        }      
+        }     
+        [HttpPost]
+        public IActionResult CalcularNota(decimal n1, decimal n2, decimal n3)
+        {
+            decimal promedio = (n1 + n2 + n3) / 3;
+            ViewBag.Promedio =decimal.Round(promedio,2);
+            return View("Index");
+        } 
     }
 }
