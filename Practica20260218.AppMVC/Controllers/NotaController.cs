494ec0dc31969e8674132b5c51334f4956a0237d
Marvin <antoniobtrigueros@gmail.com>
2026-02-18 10:42:10 -0600

    Tarea: Diseñar la pantalla de la calculadora de notas

---
+++ Practica20260218.AppMVC/Controllers/NotaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica20260218.AppMVC.Controllers
+{
+    public class NotaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }      
+    }
+}
