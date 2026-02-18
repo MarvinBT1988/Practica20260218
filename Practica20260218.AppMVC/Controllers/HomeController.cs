f2864aca3b34d8571e211d2fc531060be713f1ae
Marvin <antoniobtrigueros@gmail.com>
2026-02-18 10:20:02 -0600

    Agregar archivos de proyecto.

---
+++ Practica20260218.AppMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Practica20260218.AppMVC.Models;
+
+namespace Practica20260218.AppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practica20260218.AppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20260218.AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
