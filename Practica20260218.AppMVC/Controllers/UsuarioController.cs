b292bae1c7b6c58448b00c711f875ded36f48496
Marvin Barrera <59856529+MarvinBT1988@users.noreply.github.com>
2026-02-18 17:42:44 +0000

    SCRUM-11 Programar el controlador para hacer el calculo de notas

---
+++ Practica20260218.AppMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica20260218.AppMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }     
+    }
+}
